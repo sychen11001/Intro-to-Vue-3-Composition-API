93a5df5b7679587a6fbecaebeb3a6cdf4ba1a166
Adam Jahr <adamjahr@Adams-MacBook-Pro.local>
2025-04-01 13:58:08 -0400

    updated app.vue for appropriate starting code

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-info">
+        <h1>Socks</h1>
+      </div>
+    </div>
+  </div>
+</template>
