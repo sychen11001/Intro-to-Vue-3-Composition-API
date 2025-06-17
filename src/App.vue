32786da9229dc22d7de3b5ae6a9724cefacb2f11
sy <sychen11001@gmail.com>
2025-06-17 18:58:54 +0800

    添加练习

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { ref } from 'vue'
+import socksGreenImage from './assets/images/socks_green.jpeg'
 
 const product = ref('Socks')
-
+const image = ref(socksGreenImage)
+const href = ref('https://www.vuemastery.com/courses/intro-to-vue-3-comp-api/attribute-binding-comp-api/')
 </script>
 
 <template>
@@ -10,9 +12,14 @@ const product = ref('Socks')
     <div class="product-container">
       <div class="product-image">
         <!-- image goes here -->
+        <!-- 讲属性绑定到表达式，约等于 <img v-bind:src="{{image}}" /> -->
+         <img v-bind:src="image" />
+         <!-- 或 -->
+         <img :src="image" />
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <a :href="href">learn</a>
       </div>
     </div>
   </div>
