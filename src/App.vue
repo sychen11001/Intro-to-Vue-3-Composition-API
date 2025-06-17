c6692b16fbcea071a9038970cf776bf096f13a33
sy <sychen11001@gmail.com>
2025-06-17 18:13:54 +0800

    添加练习

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const product = ref('Boots')
+const description = ref('description')
+
+</script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-info">
-        <h1>Socks</h1>
+        <h1>{{ product }}</h1>
+        <p>{{ 'description: ' + description }}</p>
       </div>
     </div>
   </div>
