4be323357e7a01ad0e14c9b401b9259d62ddd519
sychen11001 <sychen11001@gmail.com>
2025-06-17 23:08:43 +0800

    编写练习

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,32 @@ const product = ref('Socks')
 const image = ref(socksGreenImage)
 const inStock = true
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
+const variants = ref([
+  { id: 2234, color: 'green' },
+  { id: 2235, color: 'blue' },
+])
+const sizes = ref(['S', 'M', 'L'])
 </script>
-  
+
 <template>
   <div class="nav-bar"></div>
   <div class="product-display">
     <div class="product-container">
-      <div class="product-image">    
+      <div class="product-image">
         <img v-bind:src="image">
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+        <div v-for="variant in variants" :key="variant.id">{{ variant.color }}</div>
+        <ol>
+          <!-- <li v-for="size, index in sizes" :key="index">{{ size }}</li> -->
+          <li v-for="size in sizes" :key="size">{{ size }}</li>
+        </ol>
       </div>
     </div>
   </div>
