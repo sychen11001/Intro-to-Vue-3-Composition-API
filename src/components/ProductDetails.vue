149d96a547dcfb5599cb722543ffda9c7541d537
sychen11001 <sychen11001@gmail.com>
2025-06-18 08:37:36 +0800

    编写练习

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+const details = ref(['50% cotton', '30% wool', '20% polyester'])
+
+</script>
+<template>
+    <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+</template>
