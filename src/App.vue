27833a490feaee8dca2c3667f159bec564df6c4b
sychen11001 <sychen11001@gmail.com>
2025-06-18 09:19:50 +0800

    编写练习

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,34 @@
 import { ref } from 'vue'
 import ProductDisplay from '@/components/ProductDisplay.vue'
 
-const cart = ref(0)
+// const cart = ref(0)
+const cart = ref([])
 const premium = ref(true)
 
+const updateCart = (id) => {
+  cart.value.push(id)
+}
+
+const removeFromCart = (id) => {
+  const index = cart.value.findIndex(item => item === id)
+  if (index === -1) return // 如果没有找到，直接返回
+  cart.value.splice(index, 1)
+}
+
+// splice，当没有匹配项时会返回 -1，将移除最后一个元素
+// const removeFromCart = (id) => {
+//   cart.value.splice(cart.value.indexOf(id), 1)
+// }
+
+// 会移除全部相同id的商品 
+// const removeFromCart = (id) => {
+//   cart.value = cart.value.filter((item) => item !== id)
+// }
 </script>
-  
+
 <template>
   <div class="nav-bar"></div>
   <div class="cart">Cart({{ cart }})</div>
-  <ProductDisplay :premium="premium"></ProductDisplay>
+  <div class="cart">Cart({{ cart.length }})</div>
+  <ProductDisplay :premium="premium" @add-to-cart="updateCart" @remove-from-cart="removeFromCart"></ProductDisplay>
 </template>
--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -4,83 +4,83 @@ import socksGreenImage from '@/assets/images/socks_green.jpeg'
 import socksBlueImage from '@/assets/images/socks_blue.jpeg'
 
 const props = defineProps({
-  premium: {
-    type: Boolean,
-    required: true
-  }
+    premium: {
+        type: Boolean,
+        required: true
+    }
 })
 
 const product = ref('Socks')
 const brand = ref('Vue Mastery')
 
 const selectedVariant = ref(0)
-  
+
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
 
 const variants = ref([
-  { id: 2234, color: 'green', image: socksGreenImage, quantity: 50 },
-  { id: 2235, color: 'blue', image: socksBlueImage, quantity: 0 },
+    { id: 2234, color: 'green', image: socksGreenImage, quantity: 50 },
+    { id: 2235, color: 'blue', image: socksBlueImage, quantity: 0 },
 ])
 
 const title = computed(() => {
-  return brand.value + ' ' + product.value
+    return brand.value + ' ' + product.value
 })
 
 const image = computed(() => {
-  return variants.value[selectedVariant.value].image
+    return variants.value[selectedVariant.value].image
 })
 
 const inStock = computed(() => {
-  return variants.value[selectedVariant.value].quantity > 0
+    return variants.value[selectedVariant.value].quantity > 0
 })
 
 const shipping = computed(() => {
-  if (props.premium) {
-    return 'Free'
-  }
-  else {
-    return 2.99
-  }
+    if (props.premium) {
+        return 'Free'
+    }
+    else {
+        return 2.99
+    }
 })
 
-const addToCart = () => cart.value += 1
+// const addToCart = () => cart.value += 1
+const emit = defineEmits(['add-to-cart', 'remove-from-cart'])
+const addToCart = () => {
+    emit('add-to-cart', variants.value[selectedVariant.value].id)
+}
 
 const updateVariant = (index) => {
-  selectedVariant.value = index
+    selectedVariant.value = index
+}
+
+const removeToCart = () => {
+    emit('remove-from-cart', variants.value[selectedVariant.value].id)
 }
 </script>
 
 <template>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">    
-        <img v-bind:src="image">
-      </div>
-      <div class="product-info">
-        <h1>{{ title }}</h1>
-        <p v-if="inStock">In Stock</p>
-        <p v-else>Out of Stock</p>
-        <p>Shipping: {{ shipping }}</p>
-        <ul>
-          <li v-for="detail in details">{{ detail }}</li>
-        </ul>
-        <div 
-          v-for="(variant, index) in variants" 
-          :key="variant.id"
-          @mouseover="updateVariant(index)"
-          class="color-circle"
-          :style="{ backgroundColor: variant.color }"
-        >
+    <div class="product-display">
+        <div class="product-container">
+            <div class="product-image">
+                <img v-bind:src="image">
+            </div>
+            <div class="product-info">
+                <h1>{{ title }}</h1>
+                <p v-if="inStock">In Stock</p>
+                <p v-else>Out of Stock</p>
+                <p>Shipping: {{ shipping }}</p>
+                <ul>
+                    <li v-for="detail in details">{{ detail }}</li>
+                </ul>
+                <div v-for="(variant, index) in variants" :key="variant.id" @mouseover="updateVariant(index)"
+                    class="color-circle" :style="{ backgroundColor: variant.color }">
+                </div>
+                <button class="button" :class="{ disabledButton: !inStock }" :disabled="!inStock"
+                    v-on:click="addToCart">
+                    Add to cart
+                </button>
+                <button class="button" @click="removeToCart">Remove to cart</button>
+            </div>
         </div>
-        <button
-          class="button" 
-          :class="{ disabledButton: !inStock }"
-          :disabled="!inStock"
-          v-on:click="addToCart"
-        >
-          Add to cart
-        </button>
-      </div>
     </div>
-  </div>
 </template>
